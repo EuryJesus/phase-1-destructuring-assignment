d985e9c53528f7387e895b0be7b870892d8f259e
EuryJesus <Eury0218@hotmail.com>
2023-05-25 16:03:29 -0400

    Adds index.js

--- index.js
+++ index.js
@@ -1,48 +1,35 @@
-const farmAnimals = 'cow horse sheep pig chicken';
-
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-
-const muppet = {
-  muppetName: 'Miss Piggy',
-  color: 'pink',
-  song: 'Never Before, Never Again',
-  job: 'Cast member of The Muppet Show',
-  partner: 'Kermit'
-};
-
-const nestedMuppet = {
-  nestedName: 'Kermit',
-  nestedColor: 'green',
-  album: {
-    theMuppetMovie: {
-      song1: 'Rainbow Connection',
-      song2: 'Moving Right Along',
-      song3: 'Never Before, Never Again',
-      song4: 'I Hope That Something Better Comes Along',
-    },
-  },
-  nestedJob: 'Host of The Muppet Show',
-  nestedPartner: 'Miss Piggy'
-};
-
-// Strings
-
-// 1. Use destructuring to assign appropriate variables based on the sounds animals make.
-
-// 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
-
-// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
-
-// Arrays
-
-// 4. Use destructuring to assign appropriate variables using the color names.
-
-// 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
-
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
-
-// Objects
-
-// 7. Use destructuring to assign all variables using the keys as the variable names
-
-// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+let moo = ('cow')
+let neigh = ('horse')
+let baa = ('sheep')
+let bessie = ('cow')
+let oink = ('pig')
+let dolly = ('sheep')
+let cluck = ('chicken')
+let babe = ('pig')
+let little = ('chicken')
+let blackAndWhite = ('cow')
+let black = ('sheep')
+let pink = ('pig')
+let red = ('red')
+let orange = ('orange')
+let yellow = ('yellow')
+let green = ('green')
+let blue = ('blue')
+let indigo = ('indigo')
+let violet = ('violet')
+let r = ('red')
+let o = ('orange')
+let y = ('yellow')
+let g = ('green')
+let b = ('blue')
+let v = ('violet')
+let indg = ('indigo')
+let muppetName = ('Miss Piggy')
+let color = ('pink')
+let song = ('Never Before, Never Again')
+let job = ('Cast member of The Muppet Show')
+let partner = ('Kermit')
+let song2 = ('Moving Right Along')
+let song4 = ('I Hope That Something Better Comes Along')
+let nestedJob = ('Host of The Muppet Show')
+let nestedPartner = ('Miss Piggy')
